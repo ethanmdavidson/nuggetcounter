09b718eaf70fa56f7cf9c6c23067a0af48282e71
ethanmdavidson <ethanmdavidson@gmail.com>
2018-12-01 16:36:50 -0500

    fixed trailing quote in JsCookie src tag

--- src/main/kotlin/com/machinerychorus/nuggetcounter/JsCookiePlugin.kt
+++ src/main/kotlin/com/machinerychorus/nuggetcounter/JsCookiePlugin.kt
@@ -5,7 +5,7 @@ import io.kweb.plugins.KWebPlugin
 class JsCookiePlugin : KWebPlugin() {
     override fun decorate(startHead: StringBuilder, endHead: StringBuilder) {
         startHead.appendln("""
-            <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>"
+            <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
         """.trimIndent())
     }
 }
